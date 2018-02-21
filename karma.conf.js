3f72cee44005f8c3c53b8b690ae992a7e072409c
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-02-21 07:12:15 -0300

    Run tests in IE

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,11 @@ module.exports = function (config) {
   config.set({
     frameworks: ['qunit'],
     reporters: ['spec'],
+    preprocessors: {
+      'test/qunit/**/*.js': ['babel']
+    },
     files: [
+      'node_modules/promise-polyfill/dist/polyfill.min.js',
       'node_modules/jquery/dist/jquery.min.js',
       'dist/sweetalert2.css',
       'dist/sweetalert2.js',
@@ -11,8 +15,10 @@ module.exports = function (config) {
     plugins: [
       'karma-qunit',
       'karma-spec-reporter',
+      'karma-babel-preprocessor',
       'karma-chrome-launcher',
-      'karma-firefox-launcher'
+      'karma-firefox-launcher',
+      'karma-ie-launcher'
     ]
   })
 }
